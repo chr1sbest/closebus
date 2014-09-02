189277c8a1a91e7d3c787c9fb0a11e9c897b12a7
Chris Best <chr2sbest@gmail.com>
2014-09-02 16:36:16 -0700

    redis url parser

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import os
+import urlparse
 
 API_KEY = os.environ.get('API_KEY', None)
-REDIS_URL = os.environ.get('REDISTOGO_URL', None)
+REDIS_URL = urlparse.urlparse(os.environ.get('REDISTOGO_URL', None))
 
 try:
     from local_settings import *
