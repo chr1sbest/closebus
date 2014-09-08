a3722006d31466858f3567cd797cb78845ed3372
Chris Best <chr2sbest@gmail.com>
2014-09-08 11:24:47 -0700

    merged master

--- stop_ids/NextBusData/NEXTBUS_DATA_MINER.py
+++ stop_ids/NextBusData/NEXTBUS_DATA_MINER.py
@@ -32,8 +32,19 @@ def build(agency):
             unique_dict[name].append(number)
     json.dump(unique_dict, open(agency + '.json', 'w'))
 
+def add_to_extras(g_addr, stop_ids):
+    """
+    Add new item to list of extras.
+    """
+    assert type(stop_ids) == list, 'Stop ids must be list.'
+    with open('__extras.json', 'r+') as data:
+        stops = json.load(data)
+    stops[g_addr] = stop_ids
+    json.dump(stops, open('__extras.json', 'w'))
+
 if __name__ == '__main__' :
-    all_agencies = get_all_agencies()
-    for curr_agency in all_agencies:
-        print 'Retrieving info on {0}.'.format(curr_agency)
-        build(curr_agency)
+    pass
+    #agencies = get_all_agencies()
+    #for agency in agencies:
+        #print 'Retrieving info on {0}.'.format(agency)
+        #build(agency)
--- stop_ids/bus_stops.py
+++ stop_ids/bus_stops.py
@@ -1,21 +1,19 @@
 from requests import get
-from stop_ids.stop_id_strategies \
-    import strategy_crawler, strategy_location_mapper
+from stop_id_strategies import \
+        strategy_crawler, strategy_location_mapper, strategy_extras
 
 def get_stop_id(details):
     """
     Use chain-of-command to mine for "stop_id" that corresponds
     to the specific place.
-
+   
     - Default strategy uses mined data held in json files.
     - Backup strategy scrapes for data.
+    - Final strategy queries against manual __extras file.
     """
-    # Check if all parameters exist.
-    details['stop_ids'] = "Unavailable"
-    if not details['url'] or not details['name']:
-        return {'message': 'Incomplete parameters', 'details': details}
-    # Execute chain of command to find details.
-    chain_of_command = [strategy_location_mapper, strategy_crawler]
+    chain_of_command = [
+        strategy_location_mapper, strategy_crawler, strategy_extras
+    ]
     for strategy in chain_of_command:
         details = strategy(details)
         if details['stop_ids'] != "Unavailable":
